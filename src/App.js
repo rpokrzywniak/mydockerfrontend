ae582ef05d9868ea39b283f2021cfd8f32c64bc5
Robert Pokrzywniak <robert.pokrzywniak@gmail.com>
2020-04-02 15:11:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world :D!
         </p>
         <a
           className="App-link"
